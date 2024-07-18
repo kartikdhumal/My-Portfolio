e70d2c877a7221aaab0680056ca0401d1f545a85
kartik <kartikdhumal24@gmail.com>
2024-07-19 00:07:21 +0530

    added new photo

--- src/About.js
+++ src/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import stylish from './images/IMG-20230104-WA0020.jpg'
+import stylish from './images/smile.jpg'
 import './styles/About.css'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -12,20 +12,20 @@ function About() {
   }, [])
   return (
     <div dir='ltr' className='w-100 justify-between font-medium text-lg flex-col self-center items-center '>
-       <div  className='w-100 ml-auto flex flex-col justify-start items-center font-semibold'>
-       <img src={stylish} className='stylishimg xl:w-1/3 xl:h-auto mt-10 sm:w-3/4 sm:h-2/3 rounded-full shadow-2xl grayscale' ></img>
-        <p  className='stylishimg p-6'>
-        My name is Kartik Dhumal, I'm an aspiring full stack developer based in Ahmedabad , Gujarat. I completed my Bachelors of Computer Applications at Som Lalit Institute of Computer Applications. Currently I am doing MCA from Department of Computer Science - Gujarat University </p>
+      <div className='w-100 ml-auto flex flex-col justify-start items-center font-semibold'>
+        <img src={stylish} className='stylishimg xl:w-1/3 xl:h-auto mt-10 sm:w-3/4 sm:h-2/3 rounded-full shadow-2xl grayscale' ></img>
+        <p className='stylishimg p-6'>
+          My name is Kartik Dhumal, I'm an aspiring full stack developer based in Ahmedabad , Gujarat. I completed my Bachelors of Computer Applications at Som Lalit Institute of Computer Applications. Currently I am doing MCA from Department of Computer Science - Gujarat University </p>
 
-    <p  className='stylishimg pt-1 px-6'> I have developed many types of front-ends from well know applications to Ecommerce platforms. I am eager to join a creative, problem solving team. Ability to learn and implement new technologies quickly. </p>
+        <p className='stylishimg pt-1 px-6'> I have developed many types of front-ends from well know applications to Ecommerce platforms. I am eager to join a creative, problem solving team. Ability to learn and implement new technologies quickly. </p>
 
-    <p  className='stylishimg py-6 px-6'>
-    I have successfully completed plenty of projects in which I have shown my excellence in different technologies and frameworks, I am undoubtedly good at using Full stack technologies. Currently, I am practicing Data structures & Algoridhams. Furthermore, I am an enthusiastic person and passionate about my career, and I always try to keep a high team spirit.”
-       </p>
-       </div> 
-       <div className='p-8'>
+        <p className='stylishimg py-6 px-6'>
+          I have successfully completed plenty of projects in which I have shown my excellence in different technologies and frameworks, I am undoubtedly good at using Full stack technologies. Currently, I am practicing Data structures & Algoridhams. Furthermore, I am an enthusiastic person and passionate about my career, and I always try to keep a high team spirit.”
+        </p>
+      </div>
+      <div className='p-8'>
 
-       </div>
+      </div>
     </div>
   )
 }
