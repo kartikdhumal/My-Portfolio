dbbd9c9cd43b8fb042419767ec9136a381e672d4
kartik <kartikdhumal24@gmail.com>
2024-08-08 22:07:05 +0530

    remove 2009 from education

--- src/Education.js
+++ src/Education.js
@@ -40,7 +40,7 @@ function Experience() {
                     <div dir='ltr' className='flex flex-col justify-start  rounded-lg pt-4 xl:w-8/12 sm:w-100'>
                         <h1 className='px-6 font-bold text-xl border-none'>H.S.C.</h1>
                         <p className='pt-0 px-6'> <span className='font-bold text-blue-600'>Shruti School </span>
-                            <p className='text-sm'> June 2009 - March 2020  </p>
+                            <p className='text-sm'> June 2018 - March 2020  </p>
                         </p>
                         <p className='pt-2 px-6 text-sm'>
                             Completed H.S.C in commerce stream from Shruti Higher Secondary School , Bapunagar , Ahmedabad
